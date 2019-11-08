113173c2402784c62ba0482400cb50f98361f74d
Mohammed-Sebaa <mmohamedsebaa@gmail.com>
2019-11-08 11:01:22 +0100

    Composant de citation marche

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState([]);
+
+  async function getSimpsonsQuote() {
+    const response = await fetch(
+      "https://quests.wilders.dev/simpsons-quotes/quotes"
+    );
+    const quotes = await response.json();
+    setQuote(quotes[0]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={quote.image} alt="" />
+        <h3>{quote.character}</h3>
+        <p>{quote.quote}</p>
+
+        <button onClick={getSimpsonsQuote}>Récupérer des citations</button>
       </header>
     </div>
   );
